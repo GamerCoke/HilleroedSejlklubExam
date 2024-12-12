f5f0e7e1067f8fba36686248d0c7035b84c98267
Malthe Anders Bruhn <mab017@edu.zealand.dk>
2024-12-12 12:57:53 +0100

    GitHub more like, GubHib

--- HSConsoleApp/obj/Debug/net8.0/HSConsoleApp.AssemblyInfo.cs
+++ HSConsoleApp/obj/Debug/net8.0/HSConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HSConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ec7ddfe44986bb319652e06a6b0e17c1280c7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c0c857797d1b0492ea608dc1cfab4b3907837c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HSConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HSConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HilleroedSejlklubExam/Pages/Bookings/AddBooking.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HSLibrary.Interfaces;
+using HSLibrary.Models.Dinghy;
+
+namespace HilleroedSejlklubExam.Pages.Bookings
+{
+    public class AddBookingModel : PageModel
+    {
+        private IBookingRepository _repo;
+        private IWebHostEnvironment _environment;
+
+        [BindProperty]
+        public Booking Booking { get; set; }
+
+        public AddBookingModel(IBookingRepository repo, IWebHostEnvironment environment)
+        {
+            _repo = repo;
+            _environment = environment;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
