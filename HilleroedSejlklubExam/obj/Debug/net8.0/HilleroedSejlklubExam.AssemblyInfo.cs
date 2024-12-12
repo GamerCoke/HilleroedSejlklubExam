3ad831efd8d07730435778ad7bcf1dcb96bbd41b
kaa004-zealand <kaa004@edu.zealand.dk>
2024-12-12 12:44:27 +0100

    sync

--- HilleroedSejlklubExam/obj/Debug/net8.0/HilleroedSejlklubExam.AssemblyInfo.cs
+++ HilleroedSejlklubExam/obj/Debug/net8.0/HilleroedSejlklubExam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HilleroedSejlklubExam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b95558731c76292616e3dca8e0a8d00b73020edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7daaba525a848dea0436823d8d67148064e2d5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HilleroedSejlklubExam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HilleroedSejlklubExam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
