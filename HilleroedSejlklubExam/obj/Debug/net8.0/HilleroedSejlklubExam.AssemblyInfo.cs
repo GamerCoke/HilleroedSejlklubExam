eb37f68fb95299feeef697ec29de475ddab90775
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2024-12-12 14:55:37 +0100

    he

--- HilleroedSejlklubExam/obj/Debug/net8.0/HilleroedSejlklubExam.AssemblyInfo.cs
+++ HilleroedSejlklubExam/obj/Debug/net8.0/HilleroedSejlklubExam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HilleroedSejlklubExam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a7427b1dd0628047d1dbb2931227d4b4867cf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a732df8a237828650ebf1b4aa30a1ac43d1260")]
 [assembly: System.Reflection.AssemblyProductAttribute("HilleroedSejlklubExam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HilleroedSejlklubExam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
