782e5f25f4fab78d6763f8c43dd1ca25b1fdb95b
Mikkel Barfod <mib010@edu.zealand.dk>
2024-12-12 14:57:37 +0100

    Add update to ShowBlog & Layout

--- HilleroedSejlklub/obj/Debug/net8.0/HilleroedSejlklub.AssemblyInfo.cs
+++ HilleroedSejlklub/obj/Debug/net8.0/HilleroedSejlklub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HilleroedSejlklub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb3d88e4bdc7a87d93cde2fa8f6f5e4f4db7092d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a732df8a237828650ebf1b4aa30a1ac43d1260")]
 [assembly: System.Reflection.AssemblyProductAttribute("HilleroedSejlklub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HilleroedSejlklub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
